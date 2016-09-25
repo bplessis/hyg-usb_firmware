d95662e173b0b00f3758f027092f1ef6ac7ad49d
Benoit Plessis <b.plessis@doyousoft.com>
2016-09-26 00:46:25 +0200

    Ajout d'un selection USB LowSpeed/HighSpeed. Protection des mesures par desactivation des interruptions.

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,7 @@
 #include <xc.h>
+#include <string.h>
 
+#include "types.h"
 #include "main.h"
 #include "leds.h"
 #include "usb.h"
@@ -11,9 +13,9 @@
 #pragma config FOSC = INTOSC    // * Oscillator Selection Bits
 #pragma config WDTE = OFF       // Watchdog Timer Enable
 #pragma config PWRTE = OFF      // Power-up Timer Enable
-#pragma config MCLRE = OFF      // MCLR Pin Function Select
+#pragma config MCLRE = ON       // MCLR Pin Function Select
 #pragma config CP = OFF         // Flash Program Memory Code Protection
-#pragma config BOREN = OFF      // Brown-out Reset Enable
+#pragma config BOREN = ON       // Brown-out Reset Enable
 #pragma config CLKOUTEN = OFF   // Clock Out Enable
 #pragma config IESO = OFF       // Internal/External Switchover Mode
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable
@@ -21,116 +23,161 @@
 // CONFIG2
 
 #pragma config LVP = OFF        // Low-Voltage Programming Enable (Low-voltage programming enabled)
-#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#pragma config LPBOR = ON       // Low-Power Brown Out Reset (Low-Power BOR is disabled)
 #pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-#pragma config STVREN = OFF     // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
 #pragma config PLLEN = ENABLED  // * PLL Enable Bit
 #pragma config PLLMULT = 3x     // * PLL Multipler Selection Bit
-#pragma config USBLSCLK = 24MHz // * USB Low SPeed Clock Selection bit
 #pragma config CPUDIV = CLKDIV3 // * CPU System Clock Selection Bit
 #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
 
+#if defined(_USB_LOWSPEED)
+#pragma config USBLSCLK = 24MHz // * USB Low SPeed Clock Selection bit
+#else
+#pragma config USBLSCLK = 48MHz
+#endif
+
 void interrupt interrupt_handler (  ) {
-        usb_interrupt_handler (  );
+	usb_interrupt_handler (  );
 }
 
 __INTERNAL_DEVSTATE __dev_state;
 
 void main ( void ) {
 
-        unsigned char i, j, k;
+	unsigned char i, j, k;
 
-        // Set up clock for 8MHz
+	// Set up clock for 8MHz
 
-        OSCCON = 0xF8;
+#if defined(_USB_LOWSPEED)
+	OSCCONbits.IRCF = 0b1110; /* 0v1110 = 8Mhz HFINTOSC */
+#else
+	OSCCONbits.IRCF = 0b1111; /* 0b1111 = 16MHz HFINTOSC postscalar */
 
-        // Activate LDO to supply power to the sensor ( RC5 )
+	/* Enable Active clock-tuning from the USB */
+	ACTCONbits.ACTSRC = 1; /* 1=USB */
+	ACTCONbits.ACTEN = 1;
+#endif
 
-        ANSELC = 0x00;
-        TRISCbits.TRISC5 = 0;
-        PORTCbits.RC5 = 1;
+	// setup sub systems
+	setup_leds (  );
 
-        // initialize measures
+	set_red_led ( 1 );
 
-        __dev_state.hyg.value = 0;
-        __dev_state.temp.value = 0;
+	for ( j = 0; j < 255; j++ )
+		for ( k = 0; k < 255; k++ )
+			NOP (  );
 
-        __dev_state.green_led = LED_AUTO;
-        __dev_state.red_led = LED_OFF;
-        __dev_state.yellow_led = LED_OFF;
+	// Activate LDO to supply power to the sensor ( RC5 )
+	TRISCbits.TRISC5 = 0;
+	PORTCbits.RC5 = 1;
 
-        __dev_state.fill = 0;
+	// initialize measures
+	__dev_state.hyg.value = 0;
+	__dev_state.temp.value = 0;
 
-        // setup sub systems
+	__dev_state.green_led = LED_AUTO;
+	__dev_state.red_led = LED_OFF;
+	__dev_state.yellow_led = LED_OFF;
 
-        setup_leds (  );
+	__dev_state.fill = 0;
 
-        // Animation
+	set_yellow_led ( 1 );
 
-        set_red_led ( 1 );
+	for ( j = 0; j < 255; j++ )
+		for ( k = 0; k < 255; k++ )
+			NOP (  );
 
-        for ( j = 0; j < 255; j++ )
-                for ( k = 0; k < 255; k++ )
-                        NOP (  );
+	INTCONbits.PEIE = 1;    // Enable Peripheral interrupts
+	INTCONbits.GIE = 1;     // General interrupt enabled
 
-        set_yellow_led ( 1 );
+	// Start USB
+	reset_usb (  );
 
-        for ( j = 0; j < 255; j++ )
-                for ( k = 0; k < 255; k++ )
-                        NOP (  );
+	set_green_led ( 1 );
 
-        set_green_led ( 1 );
+	for ( j = 0; j < 255; j++ )
+		for ( k = 0; k < 255; k++ )
+			NOP (  );
 
-        for ( j = 0; j < 255; j++ )
-                for ( k = 0; k < 255; k++ )
-                        NOP (  );
+	// Set GPIO
+	ANSELA = 0x00;
 
-        set_green_led ( 0 );
+	set_green_led ( 0 );
 
-        for ( j = 0; j < 255; j++ )
-                for ( k = 0; k < 255; k++ )
-                        NOP (  );
+	for ( j = 0; j < 255; j++ )
+		for ( k = 0; k < 255; k++ )
+			NOP (  );
 
-        set_yellow_led ( 0 );
+	// Measurement loop
+	OpenI2C ( MASTER, SLEW_OFF );
 
-        for ( j = 0; j < 255; j++ )
-                for ( k = 0; k < 255; k++ )
-                        NOP (  );
+	// Boot UP
+	set_yellow_led ( 0 );
 
-        set_red_led ( 0 );
+	for ( j = 0; j < 255; j++ )
+		for ( k = 0; k < 255; k++ )
+			NOP (  );
 
-        // Start USB
+	set_red_led ( 0 );
 
-        setup_usb (  );
+	while ( 1 ) {
 
-        // Measurement loop
+		INTCONbits.PEIE = 0;    // Disable Peripheral interrupts
+		INTCONbits.GIE = 0;     // Disable General interrupt
+		PIE2bits.USBIE = 0;     // Disable USB interrupt
 
-        OpenI2C ( MASTER, SLEW_OFF );
+		// Measure data
+		measure_rh ( &__dev_state.hyg );
+		measure_temp ( &__dev_state.temp );
 
-        while ( 1 ) {
+		INTCONbits.PEIE = 1;    // Enable Peripheral interrupts
+		INTCONbits.GIE = 1;     // Enable General interrupt
+		PIE2bits.USBIE = 1;     // Enable USB interrupt
 
-                measure_rh ( &__dev_state.hyg );
-                measure_temp ( &__dev_state.temp );
+		if (!usb_in_endpoint_busy(1)) {
+			// Update EP1 IN buffer with new datas.
 
-                for ( i = 0; i < 3; i++ )
-                        for ( j = 0; j < 255; j++ )
-                                for ( k = 0; k < 255; k++ )
-                                        NOP (  );
+			unsigned char*buf =  usb_get_buffer();
+			memcpy ( buf, &__dev_state,
+				 EP1_BUFLEN /*sizeof ( __dev_state )*/ );
 
-                if ( __dev_state.green_led == LED_AUTO )
-                        toggle_green_led (  );
-                else
-                        set_green_led ( __dev_state.green_led );
+			// Prepare response for next request
+			usb_send_in_buffer(1, EP1_BUFLEN);
 
-                if ( __dev_state.red_led == LED_AUTO )
-                        toggle_red_led (  );
-                else
-                        set_red_led ( __dev_state.red_led );
+			__dev_state.yellow_led == LED_OFF;
 
-                if ( __dev_state.yellow_led == LED_AUTO )
-                        toggle_yellow_led (  );
-                else
-                        set_yellow_led ( __dev_state.yellow_led );
+		} else {
+			__dev_state.yellow_led == LED_ON;
+		}
 
-        }
+
+		if ( __dev_state.green_led == LED_AUTO )
+			toggle_green_led (  );
+		else
+			set_green_led ( __dev_state.green_led );
+
+		if ( __dev_state.red_led == LED_AUTO )
+			toggle_red_led (  );
+		else
+			set_red_led ( __dev_state.red_led );
+
+		if ( __dev_state.yellow_led == LED_AUTO )
+			toggle_yellow_led (  );
+		else
+			set_yellow_led ( __dev_state.yellow_led );
+
+		// Pause
+		for ( i = 0; i < 3; i++ )
+			for ( j = 0; j < 255; j++ )
+				for ( k = 0; k < 255; k++ )
+					NOP (  );
+
+	}
 }
+
+/* Local Variables:    */
+/* mode: c             */
+/* c-basic-offset: 8   */
+/* indent-tabs-mode: t */
+/* End:                */
--- firmware/main.h
+++ firmware/main.h
@@ -4,15 +4,22 @@
 #include "si7021A10.h"
 
 typedef struct {
-        __DWORD hyg;
-        __DWORD temp;
+	__WORD hyg;
+	__WORD temp;
 
-        signed char green_led;
-        signed char yellow_led;
-        signed char red_led;
-        signed char fill;
+	signed char green_led;
+	signed char yellow_led;
+	signed char red_led;
+	uint8_t fill;
+
 } __INTERNAL_DEVSTATE;
 
 extern __INTERNAL_DEVSTATE __dev_state;
 
 #endif
+
+/* Local Variables:    */
+/* mode: c             */
+/* c-basic-offset: 8   */
+/* indent-tabs-mode: t */
+/* End:                */
